b3ef3a317eb2768b66190e2d69b9c064de0b9782
FabianRei <f.abi.an@gmx.de>
2020-10-09 20:00:42 +0200

    best theme

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -1,6 +1,6 @@
 sudo apt install zsh
 # install oh-my-zsh
-sh -c “$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # change chell to zsh
-chsh -s /usr/bin/zsh root
+# not needed chsh -s /usr/bin/zsh root
 
